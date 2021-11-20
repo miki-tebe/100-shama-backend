6ed14a14c31a350c5f07dc4202d8d4b50e3efaeb
Mikiyas Tibebu <mikitebe123@gmail.com>
2021-11-20 22:44:43 +0300

    initial commit

---
+++ config/functions/bootstrap.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = async () => {
+  try {
+    console.log("Setting up the template...");
+    console.log("Ready to go");
+  } catch (error) {
+    console.error(error);
+  }
+};
